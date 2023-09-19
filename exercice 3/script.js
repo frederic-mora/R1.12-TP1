56ac32149d9c371d860bc3e9781cefeef33deb08
Mora Frederic <frederic.mora@unilim.fr>
2023-09-19 17:43:06 +0200

    update TP1/exercice3

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -1,3 +1,16 @@
+/* 
+    Documentation : https://fr.javascript.info/
+
+    Les notions abordées dans cet exercice sont expliquées ici : 
+    . https://fr.javascript.info/logical-operators
+    . https://fr.javascript.info/ifelse
+    . https://fr.javascript.info/comparison
+
+    Objectif : 
+
+        . Approfondissement expressions booléennes
+*/
+
 /*
     Objectif : regarder ./asset/objectif.mp4
 
@@ -12,11 +25,6 @@
 
     Pensez à utiliser la console du navigateur pour tester ces fonctions et comprendre comment les utiliser.
 
-    Au besoin, quelques liens sur la documentation liée aux notions utiles à l'exercice :
-    . https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-    . https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators
-    . https://www.unilim.fr/pages_perso/frederic.mora/algorithmique/#/8/1
-
     Il y a plusieurs questions. Commentez vos réponses avant de passer à la suivante pour qu'il n'y
     ait pas d'interférence dans l'exécution de votre code.
 
@@ -24,13 +32,15 @@
 
 */
 
+let pa = getWeightOf('pantoufle');
+let pe = getWeightOf('pelote');
+let fe = getWeightOf('felix');
+let mi = getWeightOf('microcephale');
 
 /*  Q1
 
-    Déclarer une variable booléenne nommée exp1 et affectez lui une expression que le navigateur
-    évaluera à true si le plus lourd de Félix et Microcéphale est aussi plus lourd que Pantoufle et
-    plus lourd que Pelote. Faites en sorte de faire apparaître un message approprié lorsque c'est vrai.
-    Sinon on effacera le message (effacer = définir la chaîne vide '' ou "" comme message).
+    Ecrire un code qui affiche "Le plus lourd de Microcéphale et de Félix est plus lourd que Pantoufle et que Pelote"
+    lorsque c'est vrai. Et qui n'affiche rien lorsque c'est faux.
 */
 
 
@@ -40,4 +50,6 @@
     évaluera à true si le moins lourd de Félix et Microcéphale est plus lourd que Pantoufle et
     moins lourd que Pelote. Faites en sorte de faire apparaître un message approprié lorsque c'est vrai.
     Sinon on effacera le message (effacer = définir la chaîne vide '' ou "" comme message).
+    Ecrire un code qui affiche "Le moins lourd de Microcéphale et de Félix est plus lourd que Pantoufle mais moins Pelote"
+    lorsque c'est vrai. Et qui n'affiche rien lorsque c'est faux.
 */
